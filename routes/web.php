de129d7bd4473c2b225186754b59c2b5859d1c10
kazuyafaizad <kazuyafaizad@gmail.com>
2022-05-22 17:12:38 +0800

    multitenant

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\PdfController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PrintController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,32 +33,22 @@ require __DIR__ . '/auth.php';
 
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('clear/token', function () {
-        if (Auth::check() && Auth::user()->hasRole('superadmin')) {
-            Auth::user()->tokens()->delete();
-        }
-        return 'Token Cleared';
+    Route::group(['middleware' => ['restrictothers']], function () {
+        Route::resource('roles', RoleController::class)->except(['store', 'show', 'destroy']);
+        Route::resource('users', UserController::class)->except('destroy');
+        Route::post('register', [RegisteredUserController::class, 'register']);
     });
 
-    Route::get('update-password', function () {
-        return view('update-password');
-    })->name('password.edit');
 
-    Route::post('update-password', [UserController::class, 'updatePassword'])->name('password.store');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    //SUB-DOMAIN
+
     Route::domain('{portfolio}.' . env('SESSION_DOMAIN'))->name('portfolio.')->group(function () {
         Route::resource('invoice', InvoiceController::class);
         Route::resource('client', ClientController::class);
         Route::get('/invoice/repeat/{invoice_id}', [InvoiceController::class, 'repeatOrder'])->name('invoice.repeat');
         Route::get('/invoice/create/{client_id}', [InvoiceController::class, 'create'])->name('invoice.create');
         Route::get('/invoice/pdf/{invoice}', [PdfController::class, 'printInvoice'])->name('invoice.pdf');
-        Route::get('/print/invoice/', [PdfController::class, 'index'])->name('invoice.bulk.pdf');
+        Route::get('/print/invoice/', [PrintController::class, 'index'])->name('invoice.bulk.pdf');
+        Route::get('/invoice_no', [InvoiceController::class, 'invoice_no_list']);
     });
-});
-
-Route::group(['middleware' => ['auth', 'restrictothers']], function () {
-    Route::resource('roles', RoleController::class)->except(['store', 'show', 'destroy']);
-    Route::resource('users', UserController::class)->except('destroy');
-    Route::post('register', [RegisteredUserController::class, 'register']);
 });
