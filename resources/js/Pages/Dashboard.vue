2d755c78b9408491889ec39328856ac8b64d0cff
kazuyafaizad <kazuyafaizad@gmail.com>
2022-06-02 00:56:04 +0800

    add chjart

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,65 @@
-<script setup>
+<script>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
 import { Head } from '@inertiajs/inertia-vue3';
+import { Bar } from 'vue-chartjs'
+
+
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+
+export default {
+  name: 'BarChart',
+  components: {
+      BreezeAuthenticatedLayout,
+      Head,
+      Bar
+      },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart'
+    },
+    datasetIdKey: {
+      type: String,
+      default: 'label'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => {
+          'bg-base-300'
+      }
+    },
+    plugins: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [ { data: [40, 20, 12] } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
 </script>
 
 <template>
@@ -13,7 +72,7 @@ import { Head } from '@inertiajs/inertia-vue3';
             </h2>
         </template>
 
-        <div class="py-12 bg-base-300">
+        <div class="my-12 p-3 bg-base-300">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="stats stats-vertical lg:stats-horizontal w-full shadow-xl">
 
@@ -45,9 +104,19 @@ import { Head } from '@inertiajs/inertia-vue3';
             </div>
         </div>
 
-        <div class="py-12 ">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
+        <div class="my-12 ">
+            <div class="grid grid-cols-3 gap-4">
+                 <Bar
+                    :chart-options="chartOptions"
+                    :chart-data="chartData"
+                    :chart-id="chartId"
+                    :dataset-id-key="datasetIdKey"
+                    :plugins="plugins"
+                    :css-classes="cssClasses"
+                    :styles="styles"
+                    :width="width"
+                    :height="height"
+                />
             </div>
         </div>
 
